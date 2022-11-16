b4941d2c0fcc4d5de3f9abd765ea989070c40ed5
AnaJuliaVancan <ana.vancan@estudante.ifms.edu.br>
2022-11-16 16:56:16 -0400

    App realizado.

--- pwa-localiza/js/app.js
+++ pwa-localiza/js/app.js
@@ -5,3 +5,39 @@ window.addEventListener('load', e => {
     }
 }); 
 
+let posicaoInicial;
+const capturarLocalizacao = document.getElementById('localizacao');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+
+const sucesso = (posicao) => {
+    latitude.innerHTML = posicaoInicial.coords.latitude;
+    longitude.innerHTML = posicaoInicial.coords.longitude;
+};
+
+const erro = (error) => {
+    let errorMessage;
+    switch(error.code){
+        case 0: 
+              errorMessage = "Erro desconhecido"
+        break;
+
+        case 1: 
+              errorMessage = "Permissão negada"
+        break;
+
+        case 2: 
+              errorMessage = "Captura de posição indisponível!"
+        break;
+
+        case 3: 
+              errorMessage = "Tempo de solicitação excedido!"
+        break;
+
+    }
+    console.log('Ocorreu um erro: ' + errorMessage);
+};
+
+capturarLocalizacao.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(sucesso, erro);
+});
